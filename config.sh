fe3847a4ce4b343020e3c099017f487aa5f3d1c2
Jonathan Hoffman <78979371+jonnyhoffer@users.noreply.github.com>
2024-04-22 12:51:35 -0400

    Added PAT variable to config.sh

--- config.sh
+++ config.sh
@@ -3,6 +3,7 @@
 IMPORT_DIRECTORY="/Users/terskine/git/cloudcard/bulk-action-importer/new"
 DONE_DIRECTORY="/Users/terskine/git/cloudcard/bulk-action-importer/done"
 API_URL="https://api.onlinephotosubmission.com"
+PERSISTENT_ACCESS_TOKEN=""
 
 # it's best to specify the column names in the first line of the CSV
 # if you use the 'COLUMN_NAMES' option, also add the appropriate '--form' argument in the curl command in 'upload-csv.sh'
