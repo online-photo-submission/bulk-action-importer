21a3d43d65056d50ad1a790669b09a6ab71e11bf
Jonathan Hoffman <78979371+jonnyhoffer@users.noreply.github.com>
2024-04-23 00:05:18 -0400

    Added logout logic

--- importer.sh
+++ importer.sh
@@ -4,13 +4,6 @@ set -e
 
 LOG_SEPARATOR=$'\n================================================================================================\n'
 
-# get the session token from the command line
-# export SESSION_TOKEN="$1"
-# if [ -z "$SESSION_TOKEN" ]; then
-#     echo "SESSION_TOKEN is required as the 1st arg"
-#     exit 1
-# fi
-
 # get the config values from 'config.sh'
 . ./config.sh
 
@@ -29,16 +22,18 @@ if [ -z "$(ls -A $IMPORT_DIRECTORY)" ]; then
     exit 0
 fi
 
-response=$(curl -X POST ${curlUrl}/authentication-token --header 'Content-Type: application/json' --data '{"persistentAccessToken": "'$PERSISTENT_ACCESS_TOKEN'"}')
+response=$(curl -X POST ${API_URL}/authentication-token --header 'Content-Type: application/json' --data '{"persistentAccessToken": "'$PERSISTENT_ACCESS_TOKEN'"}')
 
 SESSION_TOKEN=$(echo $response | grep -o '"tokenValue":"[^"]*' | sed 's/"tokenValue":"//')
 
 # iterate over the CSVs in the import directory and upload each to the bulk action endpoint
 for FILE in $IMPORT_DIRECTORY/*.csv
 do 
-    ./upload-csv.sh $FILE $API_URL $COLUMN_NAMES
+    ./upload-csv.sh $FILE $API_URL $SESSION_TOKEN $COLUMN_NAMES
 
     mv $FILE $DONE_DIRECTORY
     echo "completed: $FILE"
     echo "$LOG_SEPARATOR"
 done
+
+curl --location ${API_URL}/person/me/logout --header 'X-Auth-Token: '$SESSION_TOKEN'' --header 'Accept: application/json' --header 'Content-Type: application/json' --data '{"authenticationToken": "'$SESSION_TOKEN'"}'
